3eaecf7b417465a65964b8f2a1bc50b53887b65b
Blocksn <kevin.schneiderf@gmail.com>
2023-09-12 19:32:33 +0200

    unnötige Using Devices entfernt
    - Einige Using Devices haben beim Erstellen eines neuen Builds Compiler Errors verursacht und wurden somit entfernt
    
    Nächstes Projekt:
    - Events für Abilitys einrichten
    - Entitys sollen Ability Events empfangen können
    - Abilitys werden bestenfalls mit der Position im Ability Array des Entitys identifiziert

--- First Game/Assets/_Scripts/Combat/Entitys/Entity.cs
+++ First Game/Assets/_Scripts/Combat/Entitys/Entity.cs
@@ -1,11 +1,7 @@
 // Base Klasse zum Erstellen eines Entitys
 // Behaviour Mode: NPC / Hosted Character / Online Character
 
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
